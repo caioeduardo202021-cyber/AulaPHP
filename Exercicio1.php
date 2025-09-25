8c12779a67dcd59a52547b5159705a6ddbfadfe8
caioeduardo202021-cyber <caioeduardo202021@gmail.com>
2025-09-25 15:02:37 -0300

    25/09

---
+++ Exercicio1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+ <h1>Exercicio de PHP</h1>   
+
+<form action="">
+
+<p>
+ Digite o nome do cliente <br>
+<input type="text" name="cliente">
+</p>
+
+<p>
+ Escolha o sexo do cliente <br>
+ <input type="radio" name="sexo" value="F">Feminino <br>
+ <input type="radio" name="sexo" value="M">Masculino <br>
+</p>
+<p>
+  Digite o valor da compra <br>
+  <input type="text" name="valorcompra">
+</p>
+<p>
+ <input type="submit" value ="Calcular">
+</p>
+</form>
+
+
+
+
+<?php  
+
+
+
+
+
+
+?>
+
+
+
+</body>
+</html>
