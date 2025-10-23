9625329e0237b0b7332ce9777c48f5d992e84470
caioeduardo202021-cyber <caioeduardo202021@gmail.com>
2025-10-23 12:26:46 -0300

    23/10

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    
+<div class="menu">
+    <div>
+    <a href="#">Aula BD</a>
+</div>
+
+<div>
+<a href="#">Cadastrar</a>
+<a href="#">Consultar</a>
+</div>
+</div>
+</body>
+</html>
